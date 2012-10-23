e41b2a0e9aaffb9f11f4890ccb22520cbc664ad8
franklsf95@gmail.com <franklsf95@gmail.com@e92cfb19-0d33-91aa-fa4e-88211fd013c6>
2012-10-23 09:36:56 +0000

    fixed +1/-1 bug

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -43,6 +43,8 @@ class Welcome extends BaseController {
 		$image = $this->getParameter('image',$this->config->item('application_prefix').'upload/portrait.jpg');
 		$name = $this->getParameter($member_type.'-name',NULL);
 		$current_school = $this->getParameter('current_school_id',NULL);
+		$school = $this->getParameter('school',NULL);
+
 		if ($account != '' && $password != '' && $member_type != '' && $email != '') {
 			$this->db->where('account',$account);
 			$member_information = $this->db->get_first('member');
@@ -63,16 +65,11 @@ class Welcome extends BaseController {
 					$data['name'] = $name;
 					$data['current_school'] = $current_school;
 					$data['email'] = $email;
-					$this->db->select('ps.name');
-					$this->db->from('public_school as ps');
-					$this->db->where('school_id',$current_school);
-					$school = $this->db->get_first();
-					$data['school_name']=$school['name'];
+					$data['school_name']=$school;
 					$this->db->insert('member',$data);
 					$member_id = $this->db->insert_id();
 					$this->db->select('m.member_id, m.account, m.member_type, m.member_type_2, m.status as member_status, m.image as member_image, m.name as member_name, m.principal, m.gender, m.birthday, m.hobby, m.qq, m.mobilephone, m.phone, m.email, m.address, m.tag, m.description, m.content, m.created_time, m.modified_time, m.current_school, m.school_name as school_name');
 					$this->db->from('member as m');
-					//$this->db->join('public_school as ps','ps.school_id = m.current_school','LEFT');
 					$this->db->where('member_id',$member_id);
 					$this->db->where('password',md5($password));
 					$member_information = $this->db->get_first();
