0f3481e757ab0eb20d6bb45a87eb4a1de3f615fd
bigbrotherhb@gmail.com <bigbrotherhb@gmail.com@e92cfb19-0d33-91aa-fa4e-88211fd013c6>
2012-11-07 15:37:32 +0000

    translated comment due to incompatible charset

--- trunk/application/config/database.php
+++ trunk/application/config/database.php
@@ -40,7 +40,7 @@
 
 $active_group = 'default';
 $active_record = TRUE;
-//这里的数据库信息在上传前需要设置为正确内容
+//database infomation here should be correctly set before uploading to the real server
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = '';
