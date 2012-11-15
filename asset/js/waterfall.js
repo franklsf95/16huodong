e03eb193f3e6d911f1127e77fe84f3fe5d68220a
franklsf95@gmail.com <franklsf95@gmail.com@e92cfb19-0d33-91aa-fa4e-88211fd013c6>
2012-11-15 05:57:03 +0000

    UI bug fixes; Waterfall working on profile page

--- asset/js/waterfall.js
+++ asset/js/waterfall.js
@@ -23,6 +23,12 @@ function updateWaterfall( json_url, wrapFunction, width ) {
         $('#waterfall-loading').show();
       },
       success: function( data ) {
+        console.log(data);
+        if( data.length==0 ) {
+          masonryHandler.append('（空）');
+          $('#waterfall-loading').slideUp();
+          return;
+        }
         var newItemStr = '';
         for(i in data)
           newItemStr += wrapFunction( i+1, data[i]);
@@ -78,4 +84,17 @@ function refillWaterfall( json_url, wrapFunction, width ) {
         masonryHandler.fadeIn();
       }
     });
+}
+
+function wrapActivity( id, act ) {
+  var str = 
+'<div class="waterfall-item" id="item-'+id+'">'+'<a href="{"activity/view"|site_url}?id='+act.activity_id+'">'+'<img src="'+act.activity_image+'" class="img-rounded"></a>'
++'<h4><a href="{"activity/view"|site_url}?id='+act.activity_id+'">'+act.activity_name+'</a></h4>'
++'<div class="waterfall-item-bottom"><ul>'
++'<li>开始时间: '+act.start_time+'</li>'
++'<li>结束时间: '+act.end_time+'</li>'
++'<li>发起人: <a href="{"profile"|site_url}?id='+act.publisher_id+'">'+act.publisher_name+'</a></li>'
++'<li><code>'+act.attend_count+'</code>人参与&nbsp;&nbsp;|&nbsp;&nbsp;<code>'+act.follow_count+'</code>人关注</li>'
++'</ul></div></div>';
+  return str;
 }
